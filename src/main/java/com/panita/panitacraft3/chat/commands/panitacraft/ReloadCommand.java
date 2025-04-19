215003bfaa259a95faed809837898ada22df116e
Javithor360 <floresmejia004@gmail.com>
2025-04-19 03:11:02 -0600

    feat: enhance reload command

--- src/main/java/com/panita/panitacraft3/chat/commands/panitacraft/ReloadCommand.java
+++ src/main/java/com/panita/panitacraft3/chat/commands/panitacraft/ReloadCommand.java
@@ -3,6 +3,7 @@ package com.panita.panitacraft3.chat.commands.panitacraft;
 import com.panita.panitacraft3.Panitacraft;
 import com.panita.panitacraft3.difficulty.DifficultyManager;
 import com.panita.panitacraft3.difficulty.DifficultyService;
+import com.panita.panitacraft3.difficulty.util.DifficultyConfig;
 import com.panita.panitacraft3.util.Global;
 import com.panita.panitacraft3.util.chat.Messenger;
 import com.panita.panitacraft3.util.commands.dynamic.AdvancedCommand;
@@ -22,8 +23,14 @@ public class ReloadCommand implements AdvancedCommand {
     public void execute(CommandSender sender, String[] args) {
         Panitacraft plugin = Panitacraft.getPlugin(Panitacraft.class);
 
-        plugin.reloadConfig();
-        Global.load(plugin);
+        plugin.reloadConfig(); // reload config.yml
+
+        // -- Global-related reloads --
+        Global.load(plugin); // reload Global variables
+
+        // -- Difficulty-related reloads --
+        DifficultyConfig.load(plugin.getConfig()); // reload difficulty config
+        DifficultyService.resetCaches(); // reset difficulty caches to ensure clean recalculation
 
         Messenger.prefixedSend(sender, "<green>Configuración recargada correctamente.</green>");
     }
